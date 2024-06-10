e59de4462bf7ae56d196f1b8a9eaa0670b3dd8c9
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2024-06-10 15:27:23 -0500

    [+] MainActivity.kt: Composicion de una interfaz con dos textos y un boton

--- app/src/main/java/dev/jcarlosj/simplejetpackcomposeapp/MainActivity.kt
+++ app/src/main/java/dev/jcarlosj/simplejetpackcomposeapp/MainActivity.kt
@@ -1,46 +1,27 @@
 package dev.jcarlosj.simplejetpackcomposeapp
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import dev.jcarlosj.simplejetpackcomposeapp.ui.theme.SimpleJetpackComposeAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            SimpleJetpackComposeAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+            Column {
+                Text(text = "Hello World!")
+                Text(text = "Good Morning")
+                Button(onClick = {
+                    Toast.makeText(applicationContext, "Button is working!", Toast.LENGTH_SHORT).show()
+                }) {
+                    Text(text = "Click me!")
                 }
             }
+
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SimpleJetpackComposeAppTheme {
-        Greeting("Android")
     }
 }
