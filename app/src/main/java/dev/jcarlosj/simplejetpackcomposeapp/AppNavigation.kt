975314f6b0cde8117357308e8b33f2c17371e525
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2024-06-12 11:43:46 -0500

    [/] AppNavigation.tk: Separa la configuracion de navegacion de la aplicacion (Finaliza Navegacion entre Screens usando Jetpack compose)

---
+++ app/src/main/java/dev/jcarlosj/simplejetpackcomposeapp/AppNavigation.kt
@@ -0,0 +1,22 @@
+package dev.jcarlosj.simplejetpackcomposeapp
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun AppNavigation() {
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = Routes.screenA ) {
+        composable( Routes.screenA ) {
+            ScreenA( navController )
+        }
+        composable( Routes.screenB + "/{name}" ) {
+            val name = it.arguments?.getString( "name" );       // Obtenemos el valor pasado por la ruta
+
+            ScreenB( "Juan", name?: "No name" )
+        }
+    }
+}
--- app/src/main/java/dev/jcarlosj/simplejetpackcomposeapp/MainActivity.kt
+++ app/src/main/java/dev/jcarlosj/simplejetpackcomposeapp/MainActivity.kt
@@ -3,27 +3,13 @@ package dev.jcarlosj.simplejetpackcomposeapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val navController = rememberNavController()
-
-            NavHost(navController = navController, startDestination = Routes.screenA ) {
-                composable( Routes.screenA ) {
-                    ScreenA( navController )
-                }
-                composable( Routes.screenB + "/{name}" ) {
-                    val name = it.arguments?.getString( "name" );       // Obtenemos el valor pasado por la ruta
-
-                    ScreenB( "Juan", name?: "No name" )
-                }
-            }
+            AppNavigation()
         }
     }
 }
