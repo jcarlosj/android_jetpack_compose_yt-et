a7a8f6e95baaee11e505bb3f7fb207b48f9b891e
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2024-06-12 10:48:01 -0500

    [+] build.gradle.kts: Implementa dependencia navigation-compose [+] Screen(A|B): Define estructura y navegacion entre ScreenA a ScreenB

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,4 +66,8 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    // https://developer.android.com/develop/ui/compose/navigation?hl=es-419#kts
+    val nav_version = "2.7.7"
+    implementation("androidx.navigation:navigation-compose:$nav_version")
 }
--- app/src/main/java/dev/jcarlosj/simplejetpackcomposeapp/MainActivity.kt
+++ app/src/main/java/dev/jcarlosj/simplejetpackcomposeapp/MainActivity.kt
@@ -3,13 +3,25 @@ package dev.jcarlosj.simplejetpackcomposeapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            LoginScreen()
+            val navController = rememberNavController()
+
+            NavHost(navController = navController, startDestination = "screen_A" ) {
+                composable( "screen_A" ) {
+                    ScreenA( navController )
+                }
+                composable( "screen_B" ) {
+                    ScreenB()
+                }
+            }
         }
     }
 }
